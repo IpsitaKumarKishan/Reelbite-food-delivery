571bc18e21b531115ea90f8ff02eba03e3a7214e
ipsita <ipsitakishan@gmail.com>
2026-07-26 14:08:36 +0530

    Rebrand project to Reelbite and configure ES module support

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -7,11 +7,11 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey:import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "vingo-food-delivery.firebaseapp.com",
-  projectId: "vingo-food-delivery",
-  storageBucket: "vingo-food-delivery.firebasestorage.app",
-  messagingSenderId: "693314883513",
-  appId: "1:693314883513:web:4840c943ecf3fb61a33068"
+  authDomain: "reelbite-food-delivery.firebaseapp.com",
+  projectId: "reelbite-food-delivery",
+  storageBucket: "reelbite-food-delivery.firebasestorage.app",
+  messagingSenderId: "533665669672",
+  appId: "1:533665669672:web:9ee8ae29c778b6bec44b1d"
 };
 
 // Initialize Firebase
